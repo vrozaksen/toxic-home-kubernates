fd8689ff5337a7e9952b2777fd7411a6bafa002c
Vrozaksen <vrozaksen@protonmail.com>
2025-01-07 21:32:44 +0100

    feat(coder): add svc_ip

--- hack/restart-all-pods.sh
+++ hack/restart-all-pods.sh
@@ -7,3 +7,6 @@ for ns in $(kubectl get ns -o jsonpath='{.items[*].metadata.name}' --context $CL
     kubectl get "${kind}" -n "${ns}" -o name  --context $CLUSTER | xargs -I {} kubectl rollout restart {} -n "${ns}" --context $CLUSTER
   done
 done
+
+# Rook fish clean:
+# for resource in cephfilesystem.ceph.rook.io/ceph-filesystem cephfilesystemsubvolumegroup.ceph.rook.io/ceph-filesystem-csi cephblockpool.ceph.rook.io/ceph-blockpool; kubectl patch $resource -n rook-ceph -p '{"metadata":{"finalizers":[]}}' --type=merge; and kubectl delete $resource -n rook-ceph; end
