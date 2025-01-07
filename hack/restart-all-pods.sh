af9ce5a718bc3d059d073626f5700171f2ac078d
Vrozaksen <vrozaksen@protonmail.com>
2025-01-07 19:35:31 +0100

    Ton of updates

--- hack/restart-all-pods.sh
+++ hack/restart-all-pods.sh
@@ -2,8 +2,8 @@
 ## This will restart all pods in all namespaces! ##
 ## Use this carefully ##
 CLUSTER=${1:-main}
-for ns in $(kubectl get ns -o jsonpath='{.items[*].metadata.name}' --context $CLUSTER); do
+for ns in $(kubectl get ns -o jsonpath='{.items[*].metadata.name}' --cluster $CLUSTER); do
   for kind in deploy daemonset statefulset; do
-    kubectl get "${kind}" -n "${ns}" -o name  --context $CLUSTER | xargs -I {} kubectl rollout restart {} -n "${ns}" --context $CLUSTER
+    kubectl get "${kind}" -n "${ns}" -o name  --cluster $CLUSTER | xargs -I {} kubectl rollout restart {} -n "${ns}" --cluster $CLUSTER
   done
 done
