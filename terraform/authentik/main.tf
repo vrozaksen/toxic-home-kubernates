9c0f8eba3bf9dc90c9148ea6d015387736c54f05
Vrozaksen <vrozaksen@protonmail.com>
2025-01-14 23:26:27 +0100

    feat(tofu): enable authentik bcs reasons

---
+++ terraform/authentik/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    authentik = {
+      source  = "goauthentik/authentik"
+      version = "2024.12.0"
+    }
+
+    bitwarden = {
+      source  = "maxlaverse/bitwarden"
+      version = ">= 0.11.0"
+    }
+  }
+}
+
+provider "bitwarden" {
+  access_token = var.bw_access_token
+  experimental {
+    embedded_client = true
+  }
+}
+
+data "bitwarden_secret" "authentik" {
+  key = "authentik"
+}
+
+locals {
+  authentik_token = regex("AUTHENTIK_TOKEN: (\\S+)", data.bitwarden_secret.authentik.value)[0]
+}
+
+provider "authentik" {
+  url   = "https://sso.${var.cluster_domain}"
+  token = local.authentik_token
+}
